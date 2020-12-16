f2e78baf5dc011e87bbeb02c5e445c84ab16e2bc
streamich <streamich@gmail.com>
2020-12-16 11:33:02 +0100

    feat: 🎸 add JSON Pointer token encoding and decoding

--- json_pointer.go
+++ json_pointer.go
@@ -5,12 +5,26 @@ import (
 	"strings"
 )
 
+// DecodeReferenceToken decodes a single JSON Pointer reference token.
+func DecodeReferenceToken(token string) string {
+	token = strings.Replace(token, `~1`, `/`, -1)
+	token = strings.Replace(token, `~0`, `~`, -1)
+	return token
+}
+
+// EncodeReferenceToken encodes a single JSON Pointer reference token.
+func EncodeReferenceToken(token string) string {
+	token = strings.Replace(token, `~`, `~0`, -1)
+	token = strings.Replace(token, `/`, `~1`, -1)
+	return token
+}
+
 // ParseJSONPointer parses JSON Pointer from canonical string form into a Go slice.
 func ParseJSONPointer(str string) ([]string, error) {
 	if len(str) == 0 {
 		return []string{}, nil
 	}
-	if !strings.HasPrefix(str, "/") {
+	if str[0] != '/' {
 		return nil, errors.New("Invalid pointer")
 	}
 	return strings.Split(str[1:], "/"), nil
--- json_pointer_test.go
+++ json_pointer_test.go
@@ -6,6 +6,52 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDecodeReferenceTokenReturnsSameStringIfThereAreNoEscapedChars(t *testing.T) {
+	decoded := DecodeReferenceToken("foobar")
+	assert.Equal(t, decoded, "foobar")
+	decoded = DecodeReferenceToken("foo/bar")
+	assert.Equal(t, decoded, "foo/bar")
+	decoded = DecodeReferenceToken("foo~bar")
+	assert.Equal(t, decoded, "foo~bar")
+}
+
+func TestDecodeReferenceTokenDecodesSpecialChars(t *testing.T) {
+	decoded := DecodeReferenceToken("foo~1bar")
+	assert.Equal(t, decoded, "foo/bar")
+	decoded = DecodeReferenceToken("foo~0bar")
+	assert.Equal(t, decoded, "foo~bar")
+	decoded = DecodeReferenceToken("~1~0foo~0bar~0~1")
+	assert.Equal(t, decoded, "/~foo~bar~/")
+	decoded = DecodeReferenceToken("~1~0")
+	assert.Equal(t, decoded, "/~")
+	decoded = DecodeReferenceToken("~0~1")
+	assert.Equal(t, decoded, "~/")
+	decoded = DecodeReferenceToken("~0")
+	assert.Equal(t, decoded, "~")
+	decoded = DecodeReferenceToken("~1")
+	assert.Equal(t, decoded, "/")
+}
+
+func TestEncodeReferenceTokenReturnsSameStringIfThereAreNoScpecialChars(t *testing.T) {
+	encoded := EncodeReferenceToken("foobar")
+	assert.Equal(t, encoded, "foobar")
+}
+
+func TestEncodeReferenceTokenEncodesSpecialChars(t *testing.T) {
+	encoded := EncodeReferenceToken("foo/bar")
+	assert.Equal(t, encoded, "foo~1bar")
+	encoded = EncodeReferenceToken("foo~bar")
+	assert.Equal(t, encoded, "foo~0bar")
+	encoded = EncodeReferenceToken("~/")
+	assert.Equal(t, encoded, "~0~1")
+	encoded = EncodeReferenceToken("/~")
+	assert.Equal(t, encoded, "~1~0")
+	encoded = EncodeReferenceToken("~")
+	assert.Equal(t, encoded, "~0")
+	encoded = EncodeReferenceToken("/")
+	assert.Equal(t, encoded, "~1")
+}
+
 func TestReturnsEmptyArrayOnRootPointer(t *testing.T) {
 	pointer, err := ParseJSONPointer("")
 	assert.Nil(t, err)
